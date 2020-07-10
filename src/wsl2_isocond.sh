1dc40ec8e1163e007a851b47503e694881641269
yuk7 <yukx00@gmail.com>
2020-07-10 21:56:50 +0900

    src: wsl2_isocond.sh: mount binfmt in container

--- src/wsl2_isocond.sh
+++ src/wsl2_isocond.sh
@@ -7,5 +7,6 @@
 
 # Mount only for container
 /usr/.bin/mount --make-private --bind /usr/.bin /usr/bin
+/usr/.bin/mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc 
 
 exec /lib/systemd/systemd --system-unit=multi-user.target
